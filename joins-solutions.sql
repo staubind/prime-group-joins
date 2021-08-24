5ce7fe1b3fa8144484eb7aa5c11fd324522e37eb
Dan St. Aubin <staubindj@gmail.com>
2021-08-24 14:02:16 -0500

    added first stretch goal

--- joins-solutions.sql
+++ joins-solutions.sql
@@ -26,3 +26,39 @@ JOIN "warehouse"
 	ON "warehouse_product"."warehouse_id" = "warehouse"."id"
 WHERE "products"."description" = 'diet pepsi';
 
+-- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT "first_name", "last_name", count(*) FROM "customers"
+JOIN "addresses"
+	ON "customers"."id" = "addresses"."customer_id"
+JOIN "orders"
+	ON "addresses"."id" = "orders"."address_id"
+GROUP BY "first_name", "last_name";
+
+
+-- How many customers do we have?
+SELECT count(*) FROM "customers";
+
+-- How many products do we carry?
+SELECT count(*) FROM "products";
+
+-- What is the total available on-hand quantity of diet pepsi?
+SELECT sum("on_hand") as "totalDietPepsi" FROM "products"
+JOIN "warehouse_product"
+	ON "products"."id" = "warehouse_product"."product_id"
+JOIN "warehouse"
+	ON "warehouse_product"."warehouse_id" = "warehouse"."id"
+WHERE "products"."description" = 'diet pepsi';
+
+----------------- STRETCH -----------------
+
+-- How much was the total cost for each order?
+SELECT 
+	"orders"."id", 
+	sum("unit_price"*"quantity") as "totalCost"
+FROM "orders"
+JOIN "line_items"
+	ON "orders"."id" = "line_items"."order_id"
+JOIN "products"
+	ON "line_items"."product_id" = "products"."id"
+GROUP BY "orders"."id"
+ORDER BY "totalCost" DESC;
